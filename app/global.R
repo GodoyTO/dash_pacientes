22adde08df05d77cf77f5cbdac2f2f3f1d995811
GodoyTO <tgodoyoliveira@gmail.com>
2026-01-30 21:43:25 -0300

    feat: adding data, logo and reorganizing the apps files

--- app/global.R
+++ app/global.R
@@ -2,14 +2,17 @@
 if(!require(shiny)) install.packages('shiny'); require(shiny)
 if(!require(shinydashboard)) install.packages('shinydashboard'); require(shinydashboard)
 if(!require(shinydashboardPlus)) install.packages('shinydashboardPlus'); require(shinydashboardPlus)
+if(!require(dashboardthemes)) install.packages('dashboardthemes'); require(dashboardthemes)
 if(!require(plotly)) install.packages('plotly'); require(plotly)
 if(!require(tidyverse)) install.packages('tidyverse'); require(tidyverse)
+
+ds <- readRDS('data/dash_ds.rds')
 
 # Color pallet ####
 pal <- c('#ffc9e5', '#ffabed', '#ff8abd', '#b8708a',
          '#e5ffc9', '#edffab', '#bdff8a', '#8ab870',
          '#cae5ff', '#acedff', '#89bbfe', '#6f8ab7',
-         '#27187e', '#758bfd', '#aeb8fe', '#ff8600', 
+         '#27187e', '#758bfd', '#aeb8fe', '#ff8600',
          '#187d2d', '#8bfc74', '#b9ffb0', '#8800ff',
          '#7d2718', '#fc748b', '#ffb0b9', '#00ff88',
          "#FFBC4D", '#2c6bb4', '#f37120', '#00c1f3')
@@ -20,10 +23,10 @@ bar_simple <- function(
         dt, x, y, cor, total, title = 'Title',
         label_x = 'Label x',
         var_text = 'Variable text') {
-    
+
     porc <- round(100*(dt[[x]] / total), 1)
-    
-    fig <- plot_ly() %>%  
+
+    fig <- plot_ly() %>%
         add_trace(
             y = dt[[y]], x = dt[[x]],
             marker = list(color = cor),
@@ -31,22 +34,22 @@ bar_simple <- function(
             type = 'bar', name = ' ',
             text = ~ paste0(dt[[y]], '\n',
                             format(dt[[x]], big.mark = '.'), ' ',
-                            var_text, '\n', 
+                            var_text, '\n',
                             format(porc, decimal.mark = ','),
-                            '%')) %>% 
+                            '%')) %>%
         layout(
             title = list(text = title, y = 0.98, x = 0.5, font = list(size = 16)),
             margin = list(pad = 1,
                           t = 40),
             yaxis = list(title = '', mirror = TRUE,
                          showgrid = FALSE,
-                         tickfont = list(size = 15)), 
+                         tickfont = list(size = 15)),
             xaxis = list(title = list(size = 16, text = rotulo_x), mirror = TRUE,
                          showgrid = TRUE, showticklabels = TRUE, zeroline = F,
                          tickfont = list(size = 15)),
-            
+
             hoverlabel = list(font = list(size = 16)))
-    
+
     return(fig)
 }
 
@@ -54,12 +57,12 @@ bar_simple <- function(
 pizzaplot2 <- function(
         df, n, label, cor = pal[1:2],
         text_n = ' subjects', title = 'Title'){
-    
+
     df$prop <- round(df[[n]]/sum(df[[n]])*100, 1)
-    
-    plot_ly() %>% 
+
+    plot_ly() %>%
         add_pie(
-            data = df, labels = ~ df[[label]], 
+            data = df, labels = ~ df[[label]],
             values = ~ df[[n]],
             sort = FALSE,
             hole = 0.5,
@@ -75,7 +78,7 @@ pizzaplot2 <- function(
                 format(df[[n]], big.mark = '.',
                        decimal.mark = ','), texto_n,
                 '\n', format(df$prop, big.mark = '.',
-                             decimal.mark = ','), '%')) %>% 
+                             decimal.mark = ','), '%')) %>%
         layout(
             title = list(
                 text = title, y = 0.97, x = 0.5, font = list(size = 16)),
